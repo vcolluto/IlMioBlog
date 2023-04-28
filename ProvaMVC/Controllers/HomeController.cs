389822858a1c8c3a058ab63eb14eb5a577e4344c
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-04-28 12:54:05 +0200

    Add project files.

---
+++ ProvaMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProvaMVC.Models;
+using System.Diagnostics;
+
+namespace ProvaMVC.Controllers
+{
+
+    //gestisce le richieste /home/*
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        //richiesta GET / oppure /home/index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //richieste GET /home/privacy
+        [HttpGet]
+        public IActionResult Privacy()
+        {
+            return View();      //restituisce la view /Home/Privacy.cshtml
+        }
+
+    
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
